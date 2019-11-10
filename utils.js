1bf4a6d64ad263b13ed11ed1646eb32f6e867c22
Vlad <vjidk018@uottawa.ca>
2019-11-10 15:28:01 -0500

    Auto stash before merge of "master" and "origin/master"

--- utils.js
+++ utils.js
@@ -65,53 +65,59 @@ function totalFactor() {
 }
 
 
-var placeSearch, autocomplete;
+var placeSearch, autocomplete1, autocomplete2;
 
   var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
+    distance: "distance"
   };
 
 function initMap() {} // now it IS a function and it is in global
 
-  function initAutocomplete() {
+  function initAutocompleteOrigin() {
     // Create the autocomplete object, restricting the search predictions to
     // geographical location types.
-    autocomplete = new google.maps.places.Autocomplete(
+    autocomplete1 = new google.maps.places.Autocomplete(
         document.getElementById('origin'), {types: ['geocode']});
 
     // Avoid paying for data that you don't need by restricting the set of
     // place fields that are returned to just the address components.
-    autocomplete.setFields(['address_component']);
+    autocomplete1.setFields(['address_component']);
 
     // When the user selects an address from the drop-down, populate the
     // address fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
   }
 
-  function fillInAddress() {
+function initAutocompleteDestination() {
+    // Create the autocomplete object, restricting the search predictions to
+    // geographical location types.
+    autocomplete2 = new google.maps.places.Autocomplete(
+        document.getElementById('destination'), {types: ['geocode']});
+
+    // Avoid paying for data that you don't need by restricting the set of
+    // place fields that are returned to just the address components.
+    autocomplete2.setFields(['address_component']);
+
+    // When the user selects an address from the drop-down, populate the
+    // address fields in the form.
+    autocomplete2.addListener('place_changed', fillInAddress);
+
+}
+
+
+
+async function fillInAddress() {
     // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
+    var place = await autocomplete1.getPlace();
+    var place2 = await autocomplete2.getPlace();
 
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-      document.getElementById(component).disabled = false;
-    }
 
-    // Get each component of the address from the place details,
-    // and then fill-in the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
-    }
-  }
+    document.getElementById('distance').innerText = await place2.latLng
+
+}
+
+function setText() {
+
+}
 
   // Bias the autocomplete object to the user's geographical location,
   // as supplied by the browser's 'navigator.geolocation' object.
